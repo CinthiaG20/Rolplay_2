c341b1024a22b80b7475833fe33f760a62cbc79d
CinthiaG20 <163062125+CinthiaG20@users.noreply.github.com>
2024-09-25 09:29:16 -0300

    Update Item.cs

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -1,34 +1,33 @@
-﻿namespace Library;
+namespace Library;
 
-public class Item : IItem
+public class Item : IItem // creo la clase Item que implementa la interfaz IItem
 {
     public string Name { get; set; }
     public int AttackValue { get; set; }
     public int DefenseValue { get; set; }
     public ItemType Type { get; set; }
 
-    public Item(string name,int valor, ItemType type)
+    public Item(string name, int valor, ItemType type) //constructor de la clase
     {
+      
         this.Name = name;
         this.Type = type;
-        if (type == ItemType.Attack)
-        {
-            AttackValue = valor;
-            DefenseValue = 0;
-        }
+      if (type == ItemType.Attack || type==ItemType.MagicAttack)
+      {
+          this.AttackValue = valor;
+          this.DefenseValue = 0;
+      }
 
-        if (type == ItemType.Defense)
-        {
-            AttackValue = 0;
-            DefenseValue = valor;
-        }
+      if (type == ItemType.Defense || type == ItemType.magicDefense)
+      {
+          this.DefenseValue = valor;
+          this.AttackValue = 0;
+      }
 
-        if (type == ItemType.Magic)
-        {
-            AttackValue = valor;
-            DefenseValue = valor;
-        }
-        
-        
+      if (type == ItemType.attackDefense || type==ItemType.Magic)
+      {
+          this.AttackValue = valor;
+          this.DefenseValue = valor;
+      }
     }
 }
